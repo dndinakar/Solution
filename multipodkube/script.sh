c7cd8f467b101ede3648865a8e64ec155a56566b
dndinakar <dndinakar@gmail.com>
2023-04-17 23:19:20 +0530

    Update script.sh

--- multipodkube/script.sh
+++ multipodkube/script.sh
@@ -2,5 +2,5 @@ kubectl apply -f getjson-service.yaml
 kubectl apply -f getjson-deployment.yaml
 kubectl apply -f readjson-service.yaml
 kubectl apply -f readjson-service-deploy.yaml
-sleep 5
+sleep 10
 kubectl port-forward svc/read-json-service 9000:9000
